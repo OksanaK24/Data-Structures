ad72509565164ef4e949db0f6179b681783773a6
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-06-17 19:36:02 -0500

    queue.py is done

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,61 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# Arrays
+# class Queue:
+#     def __init__(self):
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop(0)
+#         else:
+#             print("You can't remove anything from an empty array")
+
+# Linked Lists
+class Node:
+    def __init__(self, node = None):
+        self.node = node
+        self.next_node = None
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.head = None
+        self.tail = None
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        new_item = Node(value)
+        if self.size == 0:
+            self.head = new_item
+            self.head.next_node = new_item
+            self.tail = new_item
+        else:
+            self.tail.next_node = new_item
+            self.tail = new_item
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            print("You can't remove anything from an empty array")
+        elif self.size == 1:
+            data_to_remove = self.head.node
+            self.head.node = None
+            self.tail.node = None
+            self.size -= 1
+            return data_to_remove
+        else:
+            data_to_remove = self.head.node
+            self.head = self.head.next_node
+            self.size -= 1
+            return data_to_remove
