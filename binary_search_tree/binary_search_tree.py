003c55544e186fb19dea7e2c686cfe098d6cc9af
Oksana Kril <oksanakril@Oksanas-iMac.local>
2020-06-23 20:43:19 -0500

    binary search tree part 1 is done

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -53,7 +53,11 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right is not None:
+            self.right.for_each(fn)
+        if self.left is not None:
+            self.left.for_each(fn)
 
     # Part 2 -----------------------
 
